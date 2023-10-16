e44c3ed6a8e1afce8469bc89b7f562f683254ae3
Orum <110284222+ParkChaOrum@users.noreply.github.com>
2023-10-16 18:14:37 +0900

    Update
    Queue
    ThreadPool

--- UtilityBoxForCPP.h
+++ UtilityBoxForCPP.h
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <exception>
 #include <list>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 #define ThrowOutOfRange throw exception("Index is out of range.");
 using namespace std;
 
@@ -192,7 +195,8 @@ public:
 	}
 	~StaticArray()
 	{
-		cout << "\nStaticArray 소멸 \n";
+		delete myArray;
+		cout << "\nStaticArray 소멸\n";
 	}
 	T& operator[](int index)
 	{
@@ -445,6 +449,210 @@ public:
 		else
 		{
 			return 0;
+		}
+	}
+};
+
+template<typename T>
+class StaticQueue
+{
+public:
+	StaticQueue(int length)
+	{
+		this->length = length;
+		data = (T*)malloc(sizeof(T) * length);
+	}
+	~StaticQueue()
+	{
+		delete data;
+		cout << "\nStaticQueue 소멸\n";
+	}
+	void Enqueue(T value)
+	{
+		if (IsFull())
+		{
+			ThrowOutOfRange;
+		}
+		count++;
+		data[enqueueIndex] = value;
+		enqueueIndex = GetNextIndex(enqueueIndex);
+	}
+	T Dequeue()
+	{
+		if (IsEmpty())
+		{
+			ThrowOutOfRange;
+		}
+		count--;
+		T value = data[dequeueIndex];
+		dequeueIndex = GetNextIndex(dequeueIndex);
+		return value;
+	}
+	bool IsEmpty()
+	{
+		if (dequeueIndex == enqueueIndex && count == 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	bool IsFull()
+	{
+		if (dequeueIndex == enqueueIndex && count > 0)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	int GetLength()
+	{
+		return length;
+	}
+	int GetCount()
+	{
+		return count;
+	}
+	int GetNextIndex(int currentIndex)
+	{
+		return (currentIndex + 1) % length;
+	}
+private:
+	int length;
+	int count = 0;
+	int dequeueIndex = 0;
+	int enqueueIndex = 0;
+	T* data;
+};
+
+template<typename T>
+class DynamicInfiniteQueue : public DynamicArray<T>
+{
+public:
+	DynamicInfiniteQueue(int capacity) : DynamicArray<T>(capacity) {}
+	~DynamicInfiniteQueue()
+	{
+		cout << "\nDynamicInfiniteQueue 소멸\n";
+	}
+	void Enqueue(T& value)
+	{
+		this->Add(value);
+	}
+	void EnqueueByValue(T value)
+	{
+		this->Add(value);
+	}
+	bool IsEmpty()
+	{
+		if (dequeueIndex == this->insertIndex)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	T DequeueByValue()
+	{
+		T& value = (*this)[dequeueIndex];
+		dequeueIndex++;
+		return value;
+	}
+	T& Dequeue()
+	{
+		T& value = (*this)[dequeueIndex];
+		dequeueIndex++;
+		return value;
+	}
+private:
+	int dequeueIndex = 0;
+};
+
+class ThreadPool
+{
+public:
+	ThreadPool(int threadsCount, int queueCapacity) : taskQueue(DynamicInfiniteQueue<void(*)()>(queueCapacity)) , workerThreads(new thread[threadsCount]) , threadsCount(threadsCount)
+	{
+		runningThreadCount = threadsCount;
+		for (int i = 0; i < threadsCount; i++)
+		{
+			//workerThreads[i] = thread(&ThreadPool::Work, this);
+			workerThreads[i] = thread([this, i]() { this->Work(i); });
+		}
+	}
+	~ThreadPool()
+	{
+		WaitAllThread(true);
+	}
+	void WaitAllThread(bool andStop)
+	{
+		while (!taskQueue.IsEmpty())
+		{
+			cv.notify_all();
+		}
+		//큐에 Enqueue하는거는 오직 메인 쓰레드에서만 이뤄지고 현재 waitAllThread 함수 또한 메인 쓰레드에서만 호출됨.
+		//따라서 Enqueue 와 현재 함수 사이의 순서 관계가 보장이 됨.
+		//그래서 Empty 상태라면 큐에 있던 모든 테스크들은 이미 쓰레드들이 Dequeue해갔다는 뜻임.
+		//여기서 runningThreadCount++ 명령은 Dequeue 이전에 위치함으로 결국 Empty 상태인데 runningThreadCount == 0 이 됐다면 실제로 현재 테스크를 수행중인 쓰레드는 없다는 뜻임.
+		while (true)
+		{
+			if (runningThreadCount == 0)
+			{
+				if (andStop)
+				{
+					stopAll = true;
+					cv.notify_all();
+					for (int i = 0; i < threadsCount; i++)
+					{
+						workerThreads[i].join();
+					}
+				}
+				return;
+			}
+		}
+	}
+	void EnqueueTask(void(*task)())
+	{
+		{
+			unique_lock<mutex> mutexLock(lock);
+			taskQueue.Enqueue(task);
+		}
+		cv.notify_one();
+	}
+private:
+	DynamicInfiniteQueue<void(*)()> taskQueue;
+	thread* workerThreads;
+	int threadsCount;
+	condition_variable cv;
+	mutex lock;
+	int runningThreadCount;
+	bool stopAll = false;
+	void Work(int index)
+	{
+		while (true)
+		{
+			void(*task)() = nullptr;
+			{
+				unique_lock<mutex> mutexLock(lock);
+				while (taskQueue.IsEmpty())
+				{
+					runningThreadCount--;
+					cv.wait(mutexLock);
+					runningThreadCount++;
+					if (stopAll)
+					{
+						return;
+					}
+				}
+				task = taskQueue.Dequeue();
+			}
+			task();
 		}
 	}
 };
